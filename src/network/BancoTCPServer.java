066457d0aca8a0b923ed2fe0d77dda455c804bf9
AndresMauricioRodriguezDussan <amrodriguezd_1@uqvirtual.edu.co>
2023-03-01 17:12:04 -0500

    Corregi mala implementacion de foreach

--- src/network/BancoTCPServer.java
+++ src/network/BancoTCPServer.java
@@ -9,105 +9,96 @@ import java.util.HashMap;
 
 import model.Cuenta;
 
-
-
 public class BancoTCPServer {
 
 	public static final int PORT = 3400;
-	
+
 	private ServerSocket listener;
 	private Socket serverSideSocket;
-	
+
 	private PrintWriter toNetwork;
 	private BufferedReader fromNetwork;
-	
-	private HashMap<String,Cuenta> cuentas = new HashMap<String, Cuenta>();
-	
+
+	private HashMap<String, Cuenta> cuentas = new HashMap<String, Cuenta>();
+
 	public BancoTCPServer() {
 		System.out.println("Banco TCP server is running on port: " + PORT);
 	}
-	
-	public void init() throws Exception{
+
+	public void init() throws Exception {
 		listener = new ServerSocket(PORT);
-		
-		while(true) {
+
+		while (true) {
 			serverSideSocket = listener.accept();
-			
+
 			createStreams(serverSideSocket);
 			protocol(serverSideSocket);
 		}
 	}
-	
-	public void protocol(Socket socket) throws Exception{
-		while(true) {
+
+	public void protocol(Socket socket) throws Exception {
+		while (true) {
 			String message = fromNetwork.readLine();
-			
-			if(message.isEmpty()) {
+
+			if (message.isEmpty()) {
 				break;
 			}
-			
-			String answer="";
-			String[] control=message.split(",");
-			
+
+			String answer = "";
+			String[] control = message.split(",");
+
 			System.out.println("[Server] From Client: " + message);
-			switch(control[0]) {
-			
+			switch (control[0]) {
+
 			case "CREATE":
-				Cuenta cuenta = new Cuenta(control[1],Double.parseDouble(control[3]));
-				String nCuenta=control[2];
-				cuentas.put(nCuenta,cuenta);
-				answer="Su cuenta ha sido creada con exito, su numero de cuenta es: "+nCuenta;
+				Cuenta cuenta = new Cuenta(control[1], Double.parseDouble(control[3]));
+				String nCuenta = control[2];
+				cuentas.put(nCuenta, cuenta);
+				answer = "Su cuenta ha sido creada con exito, su numero de cuenta es: " + nCuenta;
 				break;
-				
+
 			case "CONSULT":
-				for(String llave:cuentas.keySet()) {
-					if(llave.equals(control[1])) {
-						answer="la cuenta"+llave+cuentas.get(llave).consultar();
-					}else {
-						answer="la cuenta ingresada no se encuentra registrada";
-					}
+				if (cuentas.containsKey(control[1])) {
+					answer = "la cuenta" + control[1] + cuentas.get(control[1]).consultar();
+				} else {
+					answer = "la cuenta ingresada no se encuentra registrada";
 				}
 				break;
 			case "DEPOSIT":
-				for(String llave:cuentas.keySet()) {
-					if(llave.equals(control[1])) {
-						cuentas.get(llave).depositar(Double.parseDouble(control[2]));
-						answer="la cuenta: "+llave+" tiene un nuevo saldo de: "+cuentas.get(llave).getSaldo();
-					}else {
-						answer="la cuenta ingresada no se encuentra registrada";
-					}
+				if (cuentas.containsKey(control[1])) {
+					cuentas.get(control[1]).depositar(Double.parseDouble(control[2]));
+					answer = "la cuenta: " + control[1] + " tiene un nuevo saldo de: "
+							+ cuentas.get(control[1]).getSaldo();
+				} else {
+					answer = "la cuenta ingresada no se encuentra registrada";
 				}
 				break;
 			case "RETIRE":
-				for(String llave:cuentas.keySet()) {
-					if(llave.equals(control[1])) {
-						if(cuentas.get(llave).getSaldo()>Double.parseDouble(control[2])) {
-							cuentas.get(llave).retirar(Double.parseDouble(control[2]));
-							answer="la cuenta: "+llave+" tiene un nuevo saldo de: "+cuentas.get(llave).getSaldo();
-						}else {
-							answer="saldo insuficiente para realizar el retiro";
-						}
-					}else {
-						answer="la cuenta ingresada no se encuentra registrada";
+				if (cuentas.containsKey(control[1])) {
+					if (cuentas.get(control[1]).getSaldo() > Double.parseDouble(control[2])) {
+						cuentas.get(control[1]).retirar(Double.parseDouble(control[2]));
+						answer = "la cuenta: " + control[1] + " tiene un nuevo saldo de: "
+								+ cuentas.get(control[1]).getSaldo();
 					}
+				} else {
+					answer = "la cuenta ingresada no se encuentra registrada";
 				}
 				break;
 			default:
-				answer="opcion invalida";
+				answer = "opcion invalida";
 			}
 			toNetwork.println(answer);
 		}
 	}
-	
-	private void createStreams(Socket socket) throws Exception{
-		toNetwork = new PrintWriter(socket.getOutputStream(),true);
+
+	private void createStreams(Socket socket) throws Exception {
+		toNetwork = new PrintWriter(socket.getOutputStream(), true);
 		fromNetwork = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	}
-	
-	public static void main(String args[]) throws Exception{
+
+	public static void main(String args[]) throws Exception {
 		BancoTCPServer es = new BancoTCPServer();
 		es.init();
 	}
-	
-}
 
+}
